0a5b85ded521c4fbc8713e19693cdac590b0ecdb
wrrrg <bill.gryta@gmail.com>
2018-05-05 02:47:51 -0500

    finished tests - they do delete the database though

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,8 +1,14 @@
 const expect = require("expect");
 const request = require("supertest");
 
-const { app } = require("./../server.js");
-const { Todo } = require("./../models/todo.js");
+const { app } = require("./../server/server.js");
+const { Todo } = require("./../server/models/todo.js");
+
+beforeEach(done => {
+  Todo.remove({}).then(() => {
+    done();
+  });
+});
 
 describe("POST /todos", () => {
   it("should create a new todo", done => {
@@ -10,6 +16,42 @@ describe("POST /todos", () => {
 
     request(app)
       .post("/todos")
-      .send({ text });
+      .send({ text })
+      .expect(200)
+      .expect(res => {
+        expect(res.body.text).toBe(text);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.find()
+          .then(todos => {
+            expect(todos.length).toBe(1);
+            expect(todos[0].text).toBe(text);
+            done();
+          })
+          .catch(e => done(e));
+      });
+  });
+
+  it("should not create todo with invalid body", done => {
+    request(app)
+      .post("/todos")
+      .send({})
+      .expect(400)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.find()
+          .then(todos => {
+            expect(todos.length).toBe(0);
+            done();
+          })
+          .catch(e => done(e));
+      });
   });
 });
