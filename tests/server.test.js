68c39b9af68725ba71c1b5e491c3c9d4ba5dffa6
wrrrg <bill.gryta@gmail.com>
2018-05-11 17:11:39 -0500

    delete route login tests

--- tests/server.test.js
+++ tests/server.test.js
@@ -330,3 +330,30 @@ describe("POST /users/login", () => {
       });
   });
 });
+
+describe("DELETE /users/me/token", () => {
+  it("should remove a token with a valid request", done => {
+    var token = users[0]["tokens"][0]["token"];
+
+    request(app)
+      .delete("/users/me/token")
+      .set("x-auth", token)
+      .send()
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[0]._id)
+          .then(user => {
+            console.log(user);
+            expect(user.tokens.length).toBe(0);
+            done();
+          })
+          .catch(e => {
+            done(e);
+          });
+      });
+  });
+});
